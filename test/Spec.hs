56e5c5f181a8a95bbca4a92d666f3a4c698214c3
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-04-24 13:35:24 +0200

    test(unit tests): add tests for xml
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,24 @@
 import System.Exit (exitWith, ExitCode(..))
 import Test.HUnit
 import Json
-import Xml (parseXml)
+import Xml
 import Parse (Parser(..))
 
 tests :: Test
 tests = test [
-    "test1" ~: assertEqual "Testing parseJson with null" (Right (JsonNull, "")) (runParser parseJson "null"),
-    "test2" ~: assertEqual "Testing parseJson with true" (Right ((JsonBool True), "")) (runParser parseJson "true"),
-    "test3" ~: assertEqual "Testing parseJson with false" (Right ((JsonBool False), "")) (runParser parseJson "false"),
-    "test4" ~: assertEqual "Testing parseJson with number" (Right ((JsonNumber 42), "")) (runParser parseJson "42"),
-    "test5" ~: assertEqual "Testing parseJson with string" (Right ((JsonString "Hello"), "")) (runParser parseJson "\"Hello\""),
-    "test6" ~: assertEqual "Testing parseJson with array" (Right ((JsonArray [JsonNumber 1, JsonNumber 2]), "")) (runParser parseJson "[1, 2]"),
-    "test7" ~: assertEqual "Testing parseJson with object" (Right ((JsonObject [("key", JsonString "value")]), "")) (runParser parseJson "{\"key\": \"value\"}")
+    "Json - test1" ~: assertEqual "Testing parseJson with null" (Right (JsonNull, "")) (runParser parseJson "null"),
+    "Json - test2" ~: assertEqual "Testing parseJson with true" (Right ((JsonBool True), "")) (runParser parseJson "true"),
+    "Json - test3" ~: assertEqual "Testing parseJson with false" (Right ((JsonBool False), "")) (runParser parseJson "false"),
+    "Json - test4" ~: assertEqual "Testing parseJson with number" (Right ((JsonNumber 42), "")) (runParser parseJson "42"),
+    "Json - test5" ~: assertEqual "Testing parseJson with string" (Right ((JsonString "Hello"), "")) (runParser parseJson "\"Hello\""),
+    "Json - test6" ~: assertEqual "Testing parseJson with array" (Right ((JsonArray [JsonNumber 1, JsonNumber 2]), "")) (runParser parseJson "[1, 2]"),
+    "Json - test7" ~: assertEqual "Testing parseJson with object" (Right ((JsonObject [("key", JsonString "value")]), "")) (runParser parseJson "{\"key\": \"value\"}"),
+    "Xml - test1" ~: assertEqual "Testing parseXml with simple tag" (Right ((Tag (XmlTag "tag" [Text ""] [])), "")) (runParser parseXml "<tag></tag>"),
+    "Xml - test2" ~: assertEqual "Testing parseXml with tag having text" (Right ((Tag (XmlTag "tag" [Text "Hello"] [])), "")) (runParser parseXml "<tag>Hello</tag>"),
+    "Xml - test3" ~: assertEqual "Testing parseXml with tag having attribute" (Right ((Tag (XmlTag "tag" [Text ""] [("attr", "value")])), "")) (runParser parseXml "<tag attr=\"value\"></tag>"),
+    "Xml - test4" ~: assertEqual "Testing parseXml with tag having multiple attributes" (Right ((Tag (XmlTag "tag" [Text ""] [("attr1", "value1"), ("attr2", "value2")])), "")) (runParser parseXml "<tag attr1=\"value1\" attr2=\"value2\"></tag>"),
+    "Xml - test5" ~: assertEqual "Testing parseXml with nested tags" (Right ((Tag (XmlTag "outer" [Tag (XmlTag "inner" [Text ""] [])] [])), "")) (runParser parseXml "<outer><inner></inner></outer>"),
+    "Xml - test6" ~: assertEqual "Testing parseXml with nested tags having text" (Right ((Tag (XmlTag "outer" [Tag (XmlTag "inner" [Text "Hello"] [])] [])), "")) (runParser parseXml "<outer><inner>Hello</inner></outer>")
   ]
 
 main :: IO ()
