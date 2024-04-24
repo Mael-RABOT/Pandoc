112c16ae419b50844b0689dc5f9864365abb935c
b3W <basile.fouquet@epitech.eu>
2024-04-24 19:35:38 +0200

    add(XmlConverter)
    Signed-off-by: b3W <basile.fouquet@epitech.eu>

---
+++ src/XmlToUniversalContent.hs
@@ -0,0 +1,76 @@
+{-
+-- EPITECH PROJECT, 2023
+-- B-FUN-400-LYN-4-1-mypandoc-mael.rabot
+-- File description:
+-- XmlToUniversalContent.hs
+-}
+
+module XmlToUniversalContent (xmlToUniversalContent) where
+
+import Xml (TagValue(..), XmlTag(..))
+import Types (Optional, Header(..), Item(..), Paragraph(..), UniversalContent(..), Section(..), Links(..), Text(..))
+import Data.Either (partitionEithers)
+
+findByKey :: [(String, String)] -> String -> Maybe String
+findByKey ((k, v):xs) tc | tc == k = Just v
+                            | otherwise = findByKey xs tc
+findByKey [] _ = Nothing
+
+findByName :: [TagValue] -> String -> Maybe XmlTag
+findByName [] _ = Nothing
+findByName (x:xs) name =
+    case x of
+        Tag tag ->
+            if dataTitle tag == name
+                then Just tag
+                else findByName xs name
+        _ -> findByName xs name
+
+getTagText :: Maybe XmlTag -> Maybe String
+getTagText Nothing = Nothing
+getTagText (Just (XmlTag _ ((XmlText c):_) _)) = Just c
+getTagText _ = Nothing
+
+getHeader :: Maybe XmlTag -> Either String Header
+getHeader Nothing = Left "Header not found"
+getHeader (Just tag) = case findByKey (attributes tag) "title" of
+    Nothing -> Left "Header's title field not found"
+    Just title -> Right $ Header title
+        (getTagText (findByName (dataContent tag) "author"))
+        (getTagText (findByName (dataContent tag) "date"))
+
+parapraphToItem :: [TagValue] -> Paragraph
+parapraphToItem tv = Paragraph ()
+
+tagToItem :: XmlTag -> Maybe Item
+tagToItem (XmlTag "paragraph" content attrs) = Just $ parapraphToItem content
+tagToItem _ = Nothing
+
+tagValueToItem :: TagValue -> Either String Item
+tagValueToItem (XmlText txt) = Right $ ParagraphItem (Text txt)
+tagValueToItem (Tag tag) = case tagToItem tag of
+    Nothing -> Left "Unknow tag with name: " ++ $ dataTitle tag
+    (Just item) -> Right item
+
+getBody :: Maybe XmlTag -> Either String [Item]
+getBody Nothing = Left "Body not found"
+getBody (Just tag) = processList $ map tagValueToItem $ dataContent tag
+
+convertDocument :: [TagValue] -> Either String UniversalContent
+convertDocument v = case getHeader $ findByName v "header" of
+    Left err -> Left $ "Document error: " ++ err
+    Right header -> case getBody $ findByName v "body" of
+        Left err -> Left $ "Document error: " ++ err
+        Right body -> Right $ UniversalContent header body
+
+xmlToUniversalContent :: TagValue -> Either String UniversalContent
+xmlToUniversalContent (Tag tag) = case dataTitle tag of
+    "document" -> convertDocument $ dataContent tag
+    _ -> Left "Xml docmument must begin with <document> tag"
+
+processList :: [Either String a] -> Either String [a]
+processList eithers =
+    let (lefts, rights) = partitionEithers eithers
+    in if null lefts
+       then Right rights  -- Aucune erreur, retourne la liste des valeurs
+       else Left (head lefts)
